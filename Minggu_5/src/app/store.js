ce6d5122b970d9f3b4886b043c71295a9c76e789
PokemonApiAir <103976468+PokemonApiAir@users.noreply.github.com>
2023-11-14 20:28:02 +0700

    Delete Minggu_5 directory

--- Minggu_5/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from './reduxSlice';
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    }
-})
